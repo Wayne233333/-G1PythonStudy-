02afb7e7456a84b4dea34768d20a9d695fa855b9
Wayne233333 <2900199390@qq.com>
2024-10-14 19:00:49 +0800

    202410151626

--- 实验1/T4.py
+++ 实验1/T4.py
@@ -1,38 +1,23 @@
 
 def demo():
 
-
-
     a = input("请输入一个三位整数")
 
     while(True):
 
-        l = len(a)
-
-        b = a[::-1]
-
         try:
-
             int(a)
-
         except:
-
             a = input("请输入数字，而不是字符串！")
-
             continue
 
-        if l != 3:
-
+        if len(a) != 3:
             a = input("请输入一个正确的三位整数！")
-
             continue
 
-        if a == int(b):
-
+        if a == a[::-1]:
             print(f'{a}是一个对称的三位数')
-
         else:
-
             print(f'{a}不是一个对称的三位数')
 
         break
