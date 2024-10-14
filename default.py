9fbde66696a5541c6dd1cfd7df2a15dd4bdb8d73
Wayne233333 <2900199390@qq.com>
2024-10-14 19:00:49 +0800

    202410151551

---
+++ default.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# 输入两组相同长度的数组
+x = list(map(float, input("请输入第一组数据（以空格分隔）: ").split()))
+y = list(map(float, input("请输入第二组数据（以空格分隔）: ").split()))
+
+# 检查数组长度是否相同
+if len(x) != len(y):
+    print("数组长度不相同，请重新输入。")
+else:
+    # 绘制折线图
+    plt.plot(x, y, marker='o')
+    plt.title('折线统计图')
+    plt.xlabel('第一组数据')
+    plt.ylabel('第二组数据')
+    plt.grid()
+    plt.show()
+    plt.savefig('line_chart.png')
+
+
---
+++ 实验1/T1.py
@@ -0,0 +1,13 @@
+
+def demo():
+
+    a = input("输入一个地名：")
+    b = input("输入一个人名：")
+
+    print(f'{a}很美，{b}想去看看')
+
+def main():
+    demo()
+
+if __name__ == "__main__":
+    main()
