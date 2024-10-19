ffcd092b7e7d4b2d5c59649b89bb5452ffb8ba47
Wayne233333 <2900199390@qq.com>
2024-10-19 23:55:38 +0800

    202410192355

--- U2/例2.1 输入yyyy-mm-dd输出好日子.py
+++ U2/例2.1 输入yyyy-mm-dd输出好日子.py
@@ -1,25 +1,11 @@
+
 def demo():
-    data = input('请以yyyy-mm-dd的格式输入年月日：')
-    y = data[:4]
-    m = data[5:7]
-    d = data[-2:]      # 这样把日切出来更简单，最后两个字符
 
-    if m[0] == '0':     # 注意索引访问到的是字符，右边的0必须加引号
-        m = m[-1]
+    y, m, d = map(int, input('请以yyyy-mm-dd的格式输入年月日:').split('-'))
+    print(f'{y}年{m}月{d}日是个好日子')
 
-    if d[0] == '0':     # 注意索引访问到的是字符，右边的0必须加引号
-        d = d[-1]
 
-    print(str(y) + '年' + str(m) + '月' + str(d) + '日是个好日子')
 
-demo()
+if __name__ == '__main__':
 
-def demo1():
-    data = input('请以yyyy-mm-dd的格式输入年月日：').split('-')
-    y = int(data[0])
-    m = int(data[1])
-    d = int(data[2])      
-
-    print(str(y) + '年' + str(m) + '月' + str(d) + '日是个好日子')
-
-demo1()
+    demo()
--- U2/例2.3 字数统计.py
+++ U2/例2.3 字数统计.py
@@ -1,68 +1,34 @@
-def main():
-    s='''I am a teacher. You are a student
+def demo():
+    s = '''I am a teacher. You are a student
 I am a teacher. You are a student
 I am a teacher. You are a student
 I am a teacher. You are a student I am a teacher. You are a student I am a teacher.'''
 
-    print ('='*30)
-    print ('统计信息'.center(26))
-    print ('-'*30)
+    print('=' * 30)
+    print('Information'.center(30))
+    print('-' * 30)
+
+    item_width = 25
+
+    try:
+        
+        word = s.split()
+        print('%-*s%5d' % (item_width, 'words', len(word)))
+
+        total_chars = len(s)
+        non_space_chars = total_chars - s.count(' ')
+        
+        print('%-*s%5d' % (item_width, 'words(include space)', total_chars))
+        print('%-*s%5d' % (item_width, 'words(no space)', non_space_chars))
+
+        line_count = s.count('\n') + 1
+        print('%-*s%5d' % (item_width, 'lines', line_count))
+
+    except Exception as e:
+        print("Wrong!", str(e))
     
-    item_width=25
-  
-    
-    word=s.split()
-    print ('%-*s%5d' % (item_width-2,'字数',len(word)))
+    print('-' * 30)
 
-    # 下面的循环可以用一句话实现 w_len=(sum(map(len,word)))
-    
-    w_len=0
-    for w in word:
-        w_len=w_len+len(w)
+if __name__ == '__main__':
 
- 
-    print ('%-*s%5d' % (item_width-9,'字符数（包含空格）',len(s)))
-    print ('%-*s%5d' % (item_width-10,'字符数（不包含空格）',w_len))
-
-    L=s.count('\n')+1
-    print ('%-*s%5d' % (item_width-2,'行数',L))
-    
-    print ('-'*30)
-     
-main()
-
-# 下面的代码采用全英文信息输出，对照上面的内容，看看区别在哪里
-
-def main1():
-    s='''I am a teacher. You are a student
-I am a teacher. You are a student
-I am a teacher. You are a student
-I am a teacher. You are a student I am a teacher. You are a student I am a teacher.'''
-
-    print ('='*30)
-    print ('Information'.center(30))
-    print ('-'*30)
-    
-    item_width=25
-  
-    
-    word=s.split()     # word为单词总数
-    print ('%-*s%5d' % (item_width,'words',len(word)))
-    
-##    w_len=0
-##    for w in word:       # 每个单词长度统计
-##        w_len=w_len+len(w)
-
-    # 还可以采用统计空格数的方法：
-    w_len=len(s)-s.count(' ')      # 检查这个方法跟上面方法为什么有误差，问题在哪里？(多了三个看不到的换行符）
-
- 
-    print ('%-*s%5d' % (item_width,'words(include space)',len(s)))
-    print ('%-*s%5d' % (item_width,'words(no space)',w_len))
-
-    L=s.count('\n')+1
-    print ('%-*s %5d' % (item_width,'lines',L)) # 检查这里的输出格式为什么有误，可以对照上面内容（字符串里面多了一个空格）
-    
-    print ('-'*30)
-     
-main1()
+    demo()
