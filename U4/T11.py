30bddc3af14f2c91b172304bf92e17d6916934a2
Wayne233333 <2900199390@qq.com>
2024-11-26 16:31:01 +0800

    000

--- U4/T11.py
+++ U4/T11.py
@@ -1,38 +1,13 @@
 
-def Sort(f1, f2):   #二分，查找olog2n，排序on2
-    for i in range(len(f1)):
-        t1 = f1[i]
-        t2 = f2[i]
-        l = 0
-        r = i - 1
-        while l <= r:
-            m = (l + r) // 2
-            if t1 < f1[m]:
-                r = m - 1
-            else:
-                l = m + 1
-        for j in range(i-1, l-1, -1):
-            f1[j + 1] = f1[j]
-            f2[j + 1] = f2[j]
-        if l != i:
-            f1[l] = t1
-            f2[l] = t2
-    return f1, f2
-
-num = []
-sco = []
-
+f = []
 a = input()
 
 while a != 'OVER':
-
-    b,c = map(int, a.split(' '))
-    num.append(b)
-    sco.append(c)
+    f.append(list(map(int, a.split(' '))))
     a = input()
 
-num, sco = Sort(num, sco)
+f.sort(key = lambda x: x[0])
 
-for i in range(len(num)):
-    if sco[i] >= 60:
-        print(num[i], sco[i])
+for i in range(len(f)):
+    if f[i][1] >=60:
+        print(f[i][0], f[i][1])
